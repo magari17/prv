4c4c2fd2620d54f8e71aee71ed8ad8c42586f9a7
magari17 <147991808+magari17@users.noreply.github.com>
2023-10-25 12:01:05 +0200

    Update prv.py

--- prv.py
+++ prv.py
@@ -1,8 +1,8 @@
 ## my first python code
 print("Python is fun!")
 
-num1 = 5
-print(num1, 'is of type', type(num1))
+#num1 = 5
+#print(num1, 'is of type', type(num1))
 
 num3 = 8+2j
 print(num3, 'is of type', type(num3))
