59251a557a5f6da39973cb0ea07296bcaa59b5e4
magari17 <147991808+magari17@users.noreply.github.com>
2023-10-28 12:12:32 +0200

    Update prv.py

--- prv.py
+++ prv.py
@@ -4,8 +4,8 @@ print("Python is fun!")
 num1 = 5
 print(num1, 'is of type', type(num1))
 
-num3 = 8+2j
-print(num3, 'is of type', type(num3))
+num2 = 8+2j
+print(num3, 'is of type', type(num2))
 
 num4 = [1, 2]
 print(num4)
