ff650854992f51b2b648667996bca75ab1a8c140
magari17 <147991808+magari17@users.noreply.github.com>
2023-10-28 12:16:34 +0200

    Update prv.py

--- prv.py
+++ prv.py
@@ -5,7 +5,7 @@ num1 = 5
 print(num1, 'is of type', type(num1))
 
 num2 = 8+2j
-print(num3, 'is of type', type(num2))
+print(num2, 'is of type', type(num2))
 
 num3 = [1, 2, 12]
 print(num3, 'is of type', type(num3))
